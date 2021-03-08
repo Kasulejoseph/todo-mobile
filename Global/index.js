39ee480a0cbfb40cb57aa645737ee17d32747d05
Kasulejoseph <kasule06joseph@gmail.com>
2021-03-08 05:25:13 +0300

    add functionality

---
+++ Global/index.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://todo-baxk-staging.herokuapp.com";
---
+++ screens/AppScreen.js
@@ -0,0 +1,104 @@
+import React, {useState, useEffect} from 'react';
+import {
+  Text,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  View,
+  TouchableOpacity,
+  ToastAndroid,
+} from 'react-native';
+import TodoInput from '../componets/TodoInput';
+import TodoItem from '../componets/TodoItem';
+import fetchAPI from '../helpers/FetchApi';
+
+const AppScreen = () => {
+  useEffect(async () => {
+    const data = await fetchAPI('/todos');
+    setTodoItems(data.data);
+  }, [0]);
+
+  const [todoItems, setTodoItems] = useState([]);
+
+  // Add a new item to the state
+  const addTodoItem = async (data) => {
+    return await fetchAPI('/todos/add', {
+      method: 'POST',
+      body: data,
+    })
+      .then((res) => {
+        setTodoItems([...todoItems, res.data]);
+      })
+      .catch((err) =>
+        ToastAndroid.show('an error occured', ToastAndroid.SHORT),
+      );
+  };
+
+  // Delete an item from state by index
+  const deleteTodoItem = async (index) => {
+    return await fetchAPI(`/todos/${index}`, {
+      method: 'DELETE',
+    })
+      .then(async () => {
+        const data = await fetchAPI('/todos');
+        setTodoItems(data.data);
+      })
+      .catch((err) => {
+        console.log('errr', err);
+        ToastAndroid.show('an error occured', ToastAndroid.SHORT);
+      });
+  };
+
+  // Function to set completed to true by index.
+  const completeTodoItem = async (_index) => {
+    return await fetchAPI(`/todos/${_index.id}`, {
+      method: 'PUT',
+      body: {
+        status: _index.status === 'completed' ? 'pending' : 'completed',
+      },
+    })
+      .then(async () => {
+        const data = await fetchAPI('/todos');
+        setTodoItems(data.data);
+      })
+      .catch((err) =>
+        ToastAndroid.show('an error occured', ToastAndroid.SHORT),
+      );
+  };
+
+  // Render
+  return (
+    <>
+      <SafeAreaView
+        style={{padding: 16, justifyContent: 'space-between', flex: 1}}>
+        <Text style={{fontSize: 36, fontWeight: 'bold'}}>Todo</Text>
+        {todoItems.length === 0 ? (
+          <Text style={{fontSize: 20, fontWeight: 'normal', marginTop: 23}}>
+            You got an empty list buddy 😀
+          </Text>
+        ) : null}
+
+        <FlatList
+          data={todoItems}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({item, index}) => {
+            return (
+              <TodoItem
+                item={item}
+                deleteFunction={() => {
+                  deleteTodoItem(item.id);
+                }}
+                completeFunction={() => {
+                  completeTodoItem(item);
+                }}
+              />
+            );
+          }}
+        />
+        <TodoInput onPress={addTodoItem} />
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default AppScreen;
